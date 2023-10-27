76c747fde9c15fdd92ad7034c669e993450b399a
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-10-27 12:34:48 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repositories;
+
+import com.example.demo.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT obj FROM Product obj JOIN FETCH obj.categories WHERE obj IN :products")
+    List<Product> findProductsCategories(List<Product> products);
+}
