933768bda1e7523f685b07039662c53a365a8689
lopesmfelipe <felipemlopes0009@gmail.com>
2024-05-04 13:58:01 -0400

    gitignore

--- src/main/java/com/example/demo/repositories/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entities.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-}
