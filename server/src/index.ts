32204cf1bdb12fad3bf01df8e855185c71412c1d
Felipe Lopes <felipemlopes0009@gmail.com>
2024-05-06 02:50:14 -0400

    Initializing server

---
+++ server/src/index.ts
@@ -0,0 +1,15 @@
+// felipe0999 627kbq9UVcyQ1RN1
+
+import express, { Express } from "express";
+import mongoose from "mongoose"; 
+
+const app: Express = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.json());
+
+const mongoURI: string =  
+  
+
+app.use(express.json)
