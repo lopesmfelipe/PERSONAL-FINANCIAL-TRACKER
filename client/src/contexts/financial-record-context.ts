105214cebe79d5f0e4c2340537b8638a688bf821
Felipe Lopes <felipemlopes0009@gmail.com>
2024-05-08 16:24:02 -0400

    Financial Record Context

---
+++ client/src/contexts/financial-record-context.ts
@@ -0,0 +1,36 @@
+import { createContext } from "react";
+
+
+interface FinancialRecord {
+  id?: string;
+  userId: string;
+  date: Date;
+  description: string;
+  amount: number;
+  category: string;
+  paymentMethod: string;
+}
+
+
+interface FinancialRecordsContextType {
+  records: FinancialRecord[];
+  addRecord: (record: FinancialRecord) => void;
+  updateRecord: (id: string, newRecord: Partial<FinancialRecord>)   
+}
+
+
+export const FinancialRecordsContext = createContext<>(undefined)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
