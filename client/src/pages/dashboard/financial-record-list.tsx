cfd535fde11348b4d5bffe4152e5a09a767b02d7
lopesmfelipe <felipemlopes0009@gmail.com>
2024-05-12 00:06:22 -0400

    React form

--- client/src/pages/dashboard/financial-record-list.tsx
+++ client/src/pages/dashboard/financial-record-list.tsx
@@ -1,32 +1,128 @@
-import { FinancialRecord, useFinancialRecords } from "../../contexts/financial-record-context";
+import { useMemo, useState } from "react";
+import {
+  FinancialRecord,
+  useFinancialRecords,
+} from "../../contexts/financial-record-context";
 import { useTable, Column, CellProps, Row } from "react-table";
+
+interface EditableCellProps extends CellProps<FinancialRecord> {
+  updateRecord: (rowIndex: number, columnId: string, value: any) => void;
+  editable: boolean;
+}
+
+const EditableCell: React.FC<EditableCellProps> = ({
+  value: initialValue,
+  row,
+  column,
+  updateRecord,
+  editable,
+}) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [value, setValue] = useState(initialValue);
+
+  const onBlur = () => {
+    
+  }
+
+  return (
+    <div onClick={() => editable && setIsEditing(true)}>
+      {isEditing ? (
+        <input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          autoFocus
+          onBlur={onblur}
+          style={{ width: "100%" }}
+        />
+      ) : typeof value === "string" ? (
+        value
+      ) : (
+        value.toString()
+      )}
+    </div>
+  );
+};
 
 export const FinancialRecordList = () => {
   const { records } = useFinancialRecords();
 
-  const columns : Array<Column<FinancialRecord>> = useMemo()
+  const columns: Array<Column<FinancialRecord>> = useMemo(
+    () => [
+      {
+        Header: "Description",
+        accessor: "description",
+        Cell: (props) => (
+          <EditableCell {...props} updateRecord={() => null} editable={true} />
+        ),
+      },
+      {
+        Header: "Amount",
+        accessor: "amount",
+        Cell: (props) => (
+          <EditableCell {...props} updateRecord={() => null} editable={true} />
+        ),
+      },
+      {
+        Header: "Category",
+        accessor: "category",
+        Cell: (props) => (
+          <EditableCell {...props} updateRecord={() => null} editable={true} />
+        ),
+      },
+      {
+        Header: "Payment Method",
+        accessor: "paymentMethod",
+        Cell: (props) => (
+          <EditableCell {...props} updateRecord={() => null} editable={true} />
+        ),
+      },
+      {
+        Header: "Date",
+        accessor: "date",
+        Cell: (props) => (
+          <EditableCell {...props} updateRecord={() => null} editable={false} />
+        ),
+      },
+      {
+        Header: "Delete",
+        id: "delete",
+        Cell: ({ row }) => (
+          <button onClick={() => null} className="button">
+            Delete
+          </button>
+        ),
+      },
+    ],
+    []
+  );
 
-  const {getTableProps ,getTableBodyProps, headerGroups, rows, prepareRow} = useTable({columns, data: records })
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data: records });
   return (
     <div className="table-container">
       <table {...getTableProps()} className="table">
         <thead>
-          {headerGroups.map(hg) => (
+          {headerGroups.map((hg) => (
             <tr {...hg.getHeaderGroupProps()}>
               {hg.headers.map((column) => (
-                <th {...column.getHeaderProps()}> {column.render("Header")} </th>
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
               ))}
             </tr>
-          )}
+          ))}
         </thead>
         <tbody {...getTableBodyProps}>
           {rows.map((row, idx) => {
             prepareRow(row);
-            return <tr {...row.getRowProps()}> {row.cells.map(cell) => (
-              <td {...ClerkLoaded.getCellProps()}> {cell.render("Cell")} </td>
-            )}</tr>
+            return (
+              <tr {...row.getRowProps()}>
+                {" "}
+                {row.cells.map((cell) => (
+                  <td {...cell.getCellProps()}> {cell.render("Cell")} </td>
+                ))}
+              </tr>
+            );
           })}
-          </tbody>      
+        </tbody>
       </table>
     </div>
   );
